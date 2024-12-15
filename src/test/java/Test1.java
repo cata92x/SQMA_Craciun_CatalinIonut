9d01e1f6439e5e0a29cf322399975976dd0d2307
Craciun Catalin Ionut <craciun.catalin92x@gmail.com>
2024-12-15 16:08:31 +0200

    git initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,10 @@
+// Test1.java
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class Test1 {
+    @Test
+    public void testExample1() {
+        assertTrue("This test always passes!", true);
+    }
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,10 @@
+// Test2.java
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class Test2 {
+    @Test
+    public void testExample2() {
+        assertTrue("This test always passes!", true);
+    }
+}
